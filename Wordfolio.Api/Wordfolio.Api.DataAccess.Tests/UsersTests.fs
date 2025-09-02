5eae913ec24a6e2f9d3df1165243b7d1521f224c
Kirill Gorbenko <kgorbenkodev@proton.me>
2025-09-02 11:11:38 +0300

    DataAccess testing stage 4

--- Wordfolio.Api/Wordfolio.Api.DataAccess.Tests/UsersTests.fs
+++ Wordfolio.Api/Wordfolio.Api.DataAccess.Tests/UsersTests.fs
@@ -1,16 +1,17 @@
 namespace Wordfolio.Api.DataAccess.Tests
 
-open System
 open System.Data
+open System.Data.Common
+open System.Linq
 open System.Threading
 
 open Dapper.FSharp.PostgreSQL
-open Npgsql
+open Microsoft.EntityFrameworkCore
 open Xunit
 
 open Wordfolio.Api.DataAccess
 
-type UsersTests(fixture: BaseDatabaseTestFixture) =
+ type UsersTests(fixture: BaseDatabaseTestFixture) =
   interface IClassFixture<BaseDatabaseTestFixture>
 
   [<Fact>]
@@ -18,8 +19,11 @@ type UsersTests(fixture: BaseDatabaseTestFixture) =
     fixture.WithConnectionAsync(fun (connection: IDbConnection) (transaction: IDbTransaction) (cancellationToken: CancellationToken) -> task {
       OptionTypes.register()
       do! Users.createUserAsync { Users.UserCreationParameters.Id = 123 } connection transaction cancellationToken
-      use command = new NpgsqlCommand("SELECT COUNT(*) FROM wordfolio.\"Users\" WHERE \"Id\" = 123", connection :?> NpgsqlConnection, transaction :?> NpgsqlTransaction)
-      let! countObject = command.ExecuteScalarAsync(cancellationToken)
-      let count = Convert.ToInt32(countObject)
+
+      use context: TestDbContext = createContext (connection :?> DbConnection)
+
+      let! count =
+        context.Users.Where(fun u -> u.Id = 123).CountAsync(cancellationToken)
+
       Assert.Equal(1, count)
     })
