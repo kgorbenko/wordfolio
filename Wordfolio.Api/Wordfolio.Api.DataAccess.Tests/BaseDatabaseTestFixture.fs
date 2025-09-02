76a81736b7ece9ab5455450ff6a541abd0095866
Kirill Gorbenko <kgorbenkodev@proton.me>
2025-09-02 14:24:26 +0300

    DataAccess testing stage 8

--- Wordfolio.Api/Wordfolio.Api.DataAccess.Tests/BaseDatabaseTestFixture.fs
+++ Wordfolio.Api/Wordfolio.Api.DataAccess.Tests/BaseDatabaseTestFixture.fs
@@ -21,18 +21,19 @@ open Wordfolio.Api.Migrations
     inherit DbContainerFixture<PostgreSqlBuilder, PostgreSqlContainer>(messageSink)
 
     let mutable state: {| Seeder: TestDatabaseSeeder
-                          SeederConnection: DbConnection |} option = None
+                          Connection: DbConnection |} option = None
 
     override this.Configure (builder: PostgreSqlBuilder): PostgreSqlBuilder =
         base.Configure(builder).WithImage("postgres:17.5")
 
     override _.DbProviderFactory: DbProviderFactory = NpgsqlFactory.Instance
 
-    member private this.CreateSeeder(): TestDatabaseSeeder * DbConnection =
-        let connection = this.CreateConnection()
-        connection.Open()
-        let seeder = DatabaseSeeder.create connection
-        seeder, connection
+    override this.InitializeAsync (): ValueTask =
+        // https://github.com/dotnet/fsharp/issues/2307
+        do base.InitializeAsync().GetAwaiter().GetResult()
+
+        this.EnsureInitialized()
+        ValueTask.CompletedTask
 
     member private this.EnsureMigrated() =
         let connectionString = this.Container.GetConnectionString()
@@ -54,35 +55,31 @@ open Wordfolio.Api.Migrations
     member private this.EnsureInitialized() =
         match state with
         | None ->
-            this.EnsureMigrated()
             OptionTypes.register()
-            let seeder, connection = this.CreateSeeder()
-            state <- {| Seeder = seeder
-                        SeederConnection = connection |}
+
+            let connection = this.CreateConnection()
+            connection.Open()
+
+            state <- {| Seeder = DatabaseSeeder.create connection
+                        Connection = connection |}
                       |> Some
+
+            this.EnsureMigrated()
         | Some _ -> ()
 
     member this.Seeder with get(): TestDatabaseSeeder =
-        this.EnsureInitialized()
-        match state with
-        | Some s -> s.Seeder
-        | None -> failwith "State is not initialized"
+        state.Value.Seeder
 
     member this.WithConnectionAsync
         (callback: IDbConnection -> IDbTransaction -> CancellationToken -> Task<'a>)
         : Task<'a> =
         task {
-            this.EnsureInitialized()
-
             let cancellationToken = TestContext.Current.CancellationToken
 
-            use connection = this.CreateConnection()
-            connection.Open()
-
+            let connection = state.Value.Connection
             use transaction = connection.BeginTransaction()
 
             let! result = callback connection transaction cancellationToken
-
             do! transaction.CommitAsync()
 
             return result
@@ -94,4 +91,4 @@ open Wordfolio.Api.Migrations
             | None -> ()
             | Some state ->
                 (state.Seeder :> IDisposable).Dispose()
-                (state.SeederConnection :> IDisposable).Dispose()
+                (state.Connection :> IDisposable).Dispose()
