bba86d29ef6d979c61eeb506b2404c9210a36fa4
Kirill Gorbenko <kgorbenkodev@proton.me>
2025-09-02 13:25:48 +0300

    DataAccess testing stage 7

--- Wordfolio.Api/Wordfolio.Api.DataAccess.Tests/UsersTests.fs
+++ Wordfolio.Api/Wordfolio.Api.DataAccess.Tests/UsersTests.fs
@@ -1,23 +1,43 @@
 namespace Wordfolio.Api.DataAccess.Tests
 
+open System
+open System.Threading.Tasks
+open Npgsql
 open Xunit
 
 open Wordfolio.Api.DataAccess
 
 type UsersTests(fixture: BaseDatabaseTestFixture) =
-  interface IClassFixture<BaseDatabaseTestFixture>
+    interface IClassFixture<BaseDatabaseTestFixture>
 
-  [<Fact>]
-  member _.``createUserAsync inserts a row`` () =
-      task {
-          do!
-              Users.createUserAsync { Id = 123 }
-              |> fixture.WithConnectionAsync
+    [<Fact>]
+    member _.``createUserAsync inserts a row`` () =
+        task {
+            do!
+                Users.createUserAsync { Id = 123 }
+                |> fixture.WithConnectionAsync
 
-          let! actual = fixture.Seeder |> DatabaseSeeder.getAllUsersAsync
+            let! actual = fixture.Seeder |> DatabaseSeeder.getAllUsersAsync
 
-          let expected: UserEntity list =
-              [ { Id = 123 } ]
+            let expected: UserEntity list =
+                [ { Id = 123 } ]
 
-          Assert.Equivalent(expected, actual)
-      }
+            Assert.Equivalent(expected, actual)
+        }
+
+    [<Fact>]
+    member _.``createUserAsync fails on duplicate Id`` () =
+        task {
+            do!
+                Users.createUserAsync { Id = 5 }
+                |> fixture.WithConnectionAsync
+
+            let! ex =
+                Assert.ThrowsAsync<PostgresException>(fun () ->
+                    (Users.createUserAsync { Id = 5 }
+                    |> fixture.WithConnectionAsync
+                    :> Task)
+                )
+
+            Assert.Equal("23505", ex.SqlState)
+        }
