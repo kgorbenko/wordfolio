da8019793d5438625ae046bfb9d50cb8e802bf77
Kirill Gorbenko <kgorbenkodev@proton.me>
2025-12-22 08:22:03 +0300

    Avoid register and login API calls in functional tests

--- Wordfolio.Api/Wordfolio.Api.Tests/WebApplicationFactory.fs
+++ Wordfolio.Api/Wordfolio.Api.Tests/WebApplicationFactory.fs
@@ -1,8 +1,15 @@
 ﻿namespace Wordfolio.Api.Tests
 
+open System.Net.Http
+open System.Net.Http.Headers
+open System.Threading.Tasks
+
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Mvc.Testing
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+
+open Wordfolio.Api.Identity
 
 type WebApplicationFactory(connectionString: string) =
     inherit WebApplicationFactory<Program.Program>()
@@ -13,3 +20,22 @@ type WebApplicationFactory(connectionString: string) =
 
         builder.UseSetting("ConnectionStrings:wordfoliodb", connectionString)
         |> ignore
+
+        builder.ConfigureServices(fun services ->
+            services.AddScoped<ITestTokenGenerator, TestTokenGenerator>()
+            |> ignore)
+        |> ignore
+
+    member this.CreateAuthenticatedClientAsync(user: User) : Task<HttpClient> =
+        task {
+            use scope = this.Services.CreateScope()
+
+            let tokenGenerator =
+                scope.ServiceProvider.GetRequiredService<ITestTokenGenerator>()
+
+            let! token = tokenGenerator.GenerateAccessTokenAsync(user)
+
+            let client = this.CreateClient()
+            client.DefaultRequestHeaders.Authorization <- AuthenticationHeaderValue("Bearer", token)
+            return client
+        }
